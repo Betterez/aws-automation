840a428ad43b7edda6b86ec1fcd8f90becb93e43
Tal <tal@talreg.com>
2018-03-14 18:07:37 -0400

    update tests

--- test/mocks/SecurityCheckerMock.rb
+++ test/mocks/SecurityCheckerMock.rb
@@ -11,20 +11,20 @@ class SecurityCheckerMock < SecurityChecker
     @all_users_keys.keys.each do |username|
       @all_users_keys[username].each do |user_entry|
         if !user_entry[:usage].nil? then
-          user_entry[:usage]=DateTime.parse(user_entry[:usage])
+          user_entry[:usage]=Time.parse(user_entry[:usage])
         end
         if !user_entry[:created_date].nil? then
-          user_entry[:created_date]=DateTime.parse(user_entry[:created_date])
+          user_entry[:created_date]=Time.parse(user_entry[:created_date])
         end
       end
     end
 
     @keys_data_index.keys.each do |key_name|
       if (!@keys_data_index[key_name][:usage].nil?)
-        @keys_data_index[key_name][:usage]=DateTime.parse(@keys_data_index[key_name][:usage])
+        @keys_data_index[key_name][:usage]=Time.parse(@keys_data_index[key_name][:usage])
       end
       if (!@keys_data_index[key_name][:created_date].nil?)
-        @keys_data_index[key_name][:created_date]=DateTime.parse(@keys_data_index[key_name][:created_date])
+        @keys_data_index[key_name][:created_date]=Time.parse(@keys_data_index[key_name][:created_date])
       end
     end
 
@@ -39,7 +39,7 @@ class SecurityCheckerMock < SecurityChecker
     return {
       access_key: {
       access_key_id: "AKIAIOSFODNN7EXAMPLE",
-      create_date: DateTime.now,
+      create_date: Time.now,
       secret_access_key: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY",
       status: "Active",
       user_name: username,
