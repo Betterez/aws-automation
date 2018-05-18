08053b9366d606ebbb6321cc39d752af2903f017
Tal Riginiano <tal@talreg.com>
2018-05-18 11:00:25 -0400

    added vault runner

---
+++ run_with_vault_vars.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/ruby
+require_relative './betterez/VaultDriver'
+require 'optparse'
+require 'open3'
+
+runner_options={}
+OptionParser.new do |opts|
+  opts.banner="usage #{__FILE__} [options]"
+  opts.on("--env ENVIRONMENT","environment type - staging,sandbox,production") do |argument|
+    runner_options[:environment]=argument
+  end
+  opts.on("--command COMMAND","command to execute") do |argument|
+    runner_options[:command]=argument
+  end
+  opts.on("--repo REPOSITORY","repository to run against") do |argument|
+    runner_options[:repo]=argument
+  end
+end.parse!
+raise OptionParser::MissingArgument if (  (runner_options[:environment] == nil )||( runner_options[:environment] == "" ) )
+raise OptionParser::MissingArgument if (  (runner_options[:command] == nil )||( runner_options[:command] == "" ) )
+raise OptionParser::MissingArgument if (  (runner_options[:repo] == nil )||( runner_options[:repo] == "" ) )
+
+
+puts "for environment #{runner_options[:environment]}"
+puts "running: #{runner_options[:command]}"
+puts "=========================="
+driver = VaultDriver.from_secrets_file(runner_options[:environment])
+vars= driver.get_system_variables_for_service(runner_options[:repo])
+Open3.popen3("#{vars} #{runner_options[:command]}") do |stdin,stdout,stderr|
+  if !stderr.nil? && stderr.read!=""
+    puts "error:#{stderr.read }"
+  else
+    puts stdout.read
+  end
+end
