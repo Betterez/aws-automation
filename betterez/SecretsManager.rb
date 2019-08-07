7213b00d29a014fa287533a778ad8d6b406d523f
Tal Riginiano <tal@talreg.com>
2019-08-07 09:42:58 -0400

    update

--- betterez/SecretsManager.rb
+++ betterez/SecretsManager.rb
@@ -3,9 +3,11 @@ class SecretsManager
         @default_engine="aws"
         @environment="production"
     end
+    
     def get_engine
         @default_engine
     end    
+    
     attr_accessor :environment
     attr_accessor :repository
 end
