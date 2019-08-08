ba08732e04e754d0030e744f352457b930191afb
Tal Riginiano <tal@talreg.com>
2019-08-08 14:20:04 -0400

    update

--- betterez/SecretsManager.rb
+++ betterez/SecretsManager.rb
@@ -1,39 +1,70 @@
+# frozen_string_literal: true
+
 require 'aws-sdk-secretsmanager'
 require 'base64'
 class SecretsManager
-    NO_REPO=:"No repository"
-    NO_ENV=:"No environment"
-    def initialize
-        @default_engine="aws"
-        @environment="production"
-        region_name = "us-east-1"
-        @client = Aws::SecretsManager::Client.new(region: region_name)
-    end
+  NO_REPO = :"No repository"
+  NO_ENV = :"No environment"
+  def initialize
+    @default_engine = 'aws'
+    @environment = 'production'
+    region_name = 'us-east-1'
+    @client = Aws::SecretsManager::Client.new(region: region_name)
+  end
 
-    def get_engine
-        @default_engine
-    end
+  def get_engine
+    @default_engine
+  end
 
-    def get_json
-        throw  SecretsManager::NO_ENV if  @environment.nil?
-        throw SecretsManager::NO_REPO if @repository.nil?
-        secret_name = compose_secret_name
-        begin
-          get_secret_value_response = @client.get_secret_value(secret_id: secret_name)
-        rescue => e
-          return nil,500
-        end
-        if get_secret_value_response.secret_string
-          secret = get_secret_value_response.secret_string
-          json_secret=JSON.parse secret
-        end
-        return json_secret,200
+  def get_json
+    throw SecretsManager::NO_ENV if @environment.nil?
+    throw SecretsManager::NO_REPO if @repository.nil?
+    secret_name = compose_secret_name
+    begin
+      get_secret_value_response = @client.get_secret_value(secret_id: secret_name)
+    rescue StandardError => e
+      return nil, 500
     end
-    attr_accessor :environment
-    attr_accessor :repository
-    private
-    def compose_secret_name
-      @repository+@environment
+    if get_secret_value_response.secret_string
+      secret = get_secret_value_response.secret_string
+      json_secret = JSON.parse secret
     end
-    # private:
+    [json_secret, 200]
+  end
+
+
+  def set_secret_value(_secret_hash)
+    resp=@client.create_secret({
+      client_request_token: "token1token1token1token1token1token1token1",
+      name: compose_secret_name,
+      secret_binary:nil,
+      secret_string: _secret_hash[:value]
+      })
+    200
+  end
+
+  def get_all_secrets_names
+    secrets_name_result=[]
+    resp=@client.list_secrets()
+    resp[:secret_list].each do |item|
+      secrets_name_result.push(item[:name])
+    end
+    secrets_name_result
+  end
+
+  def need_to_create_secret(secret_name)
+    names=get_all_secrets_names
+    return !names.include?(secret_name)
+  end
+
+  attr_accessor :environment
+  attr_accessor :repository
+
+  private
+
+  def compose_secret_name
+    @repository + @environment
+  end
+
+
 end
--- test/SecretsManagerTest.rb
+++ test/SecretsManagerTest.rb
@@ -1,45 +1,54 @@
 #!/usr/bin/ruby
-require "test/unit"
-require_relative "../betterez/SecretsManager"
+# frozen_string_literal: true
+
+require 'test/unit'
+require_relative '../betterez/SecretsManager'
 
 class SecretsManagerTest < Test::Unit::TestCase
-    def setup
-        @manager=SecretsManager.new
-    end
-    private
-    def compose_secret_name
-    end
-    public
-    def test_default_engine
-        assert_equal(@manager.get_engine,"aws")
-    end
+  def setup
+    @manager = SecretsManager.new
+  end
 
-    def test_initial_environemnt
-        assert_equal(@manager.environment,"production")
-    end
+  def test_default_engine
+    assert_equal(@manager.get_engine, 'aws')
+  end
 
-    def test_initial_repository
-        assert_equal(@manager.repository,nil)
-    end
+  def test_initial_environemnt
+    assert_equal(@manager.environment, 'production')
+  end
 
-    def test_setting_repository
-        @manager.repository="app"
-        assert_equal(@manager.repository,"app","repository should be assignable")
-    end
-    def test_get_json_throws_without_repo
-        setup
-        assert_throw(SecretsManager::NO_REPO) do
-            @manager.get_json
-        end
-    end
-    def test_get_json
-        # omit
-        setup
-        @manager.repository="btrz-api-loyalty"
-        data,code=@manager.get_json
-        assert_equal(code,200)
-        assert_equal(data["username"],"loyalty_user")
-    end
+  def test_initial_repository
+    assert_equal(@manager.repository, nil)
+  end
 
-    # private compose_secret_name
+  def test_setting_repository
+    @manager.repository = 'app'
+    assert_equal(@manager.repository, 'app', 'repository should be assignable')
+  end
+
+  def test_get_json_throws_without_repo
+    setup
+    assert_throw(SecretsManager::NO_REPO) do
+      @manager.get_json
+    end
+  end
+
+  def test_get_json
+    omit
+    @manager.repository = 'btrz-api-loyalty'
+    data, code = @manager.get_json
+    assert_equal(code, 200)
+    assert_equal(data['username'], 'loyalty_user')
+  end
+
+  def test_secret_creation_requires
+    assert_false(@manager.need_to_create_secret("testtest"))
+  end
+
+  def test_set_variable
+    @manager.repository = 'test'
+    @manager.environment = 'test'
+    code = @manager.set_secret_value(name: 'mytest', secret: '123456789')
+    assert(code > 200 || code < 400)
+  end
 end
