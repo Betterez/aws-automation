fc58586b46a71d1d2ea94293c0b6ff3a1deb01aa
Fblind <fblind@gmail.com>
2019-10-11 12:42:06 -0300

    'add params to rollback lambda script'

--- add_rollback_lambda_version.rb
+++ add_rollback_lambda_version.rb
@@ -3,8 +3,18 @@ require 'aws-sdk'
 @region = 'us-east-1' 
 @client = Aws::Lambda::Client.new(region: @region)
 
+runner_options = { timeout: 3700, ignore_errors: false }
+STDOUT.sync = true
+OptionParser.new do |opts|
+  opts.banner = "usage #{__FILE__} [options]"
+  opts.on('--function_name FUNTION_NAME', 'find in the claudia.json file of the project') do |argument|
+    runner_options[:function_name] = argument
+  end
+end.parse!
+raise OptionParser::MissingArgument if runner_options[:function_name].nil? || (runner_options[:function_name] == '')
+
 repo = @client.get_alias({
-  function_name: "btrz-applet-loyalty",
+  function_name: runner_options[:function_name]
   name: "latest"
 })
 
