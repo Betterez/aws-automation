ef4ae8be771a71dd3ed097a030f01bc8fb713f70
Marc Peters <mr.marc.peters@gmail.com>
2026-08-07 14:00:43 -0700

    [MP] #86bbajdwr During AMI creation, ensure that the instance used to build the AMI does not have the 'Online=yes' tag.  This is done so that we can use the 'Online' tag to control whether or not we publish monitoring data for the server.  We do not want to publish monitoring data for servers that are being used to create AMIs, because those servers are not part of the 'live' fleet, and they distort our fleet monitoring data.

--- betterez/AwsInstance.rb
+++ betterez/AwsInstance.rb
@@ -861,7 +861,7 @@ class AwsInstance
       end
       notifire.notify(1, 'updating build number')
       aws_instance.update_build_number(service_setup_data)
-      if service_setup_data[:offline_mode]
+      if service_setup_data[:offline_mode] || service_setup_data[:ami]
         aws_instance.update_tag_value('Online','no')
       else
         aws_instance.update_tag_value('Online','yes')
